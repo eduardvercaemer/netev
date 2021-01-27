6c0d57319f7b14d28b6788a57a69a50867708aa9
Eduard Vercaemer <vercaemereduard@gmail.com>
2021-01-27 01:16:14 -0600

    add explanation to example

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,12 +3,23 @@ extern crate netev;
 fn main() {
     /* usage example */
 
-    /* create new poller at given port */
+    /*
+       we create a network event listener, binded to a UDP port,
+       8000 in this case
+
+       it spawns a slave thread that populates an event queue based
+       on packets it receives in the port
+    */
     let mut poller = netev::EvMaster::new("8000");
 
     /* game loop */
     loop {
-        /* get a single event from poller */
+        /*
+           we can then use the `next` method in the master thread, to
+           ask the queue for the next event, yielding a Some(e) with
+           the event if there is one (popping it from the queue), or
+           None if there are none at the moment
+        */
         if let Some(e) = poller.next() {
             println!("got event: {:?}", e);
         }
--- examples/simple_dbg.rs
+++ examples/simple_dbg.rs
@@ -1,7 +1,16 @@
 use std::net::UdpSocket;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
+    /*
+       we can use this simple program to send dummy packets
+       to our program to test the events it produces
+    */
     let sock = UdpSocket::bind("localhost:34254").unwrap();
     sock.connect("localhost:8000").unwrap();
-    sock.send(&[0, 2, 3]).unwrap();
+    loop {
+        sock.send(&[0, 2, 3]).unwrap();
+        thread::sleep(Duration::from_secs(1));
+    }
 }
